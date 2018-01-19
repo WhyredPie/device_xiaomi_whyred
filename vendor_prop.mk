db3a07b92d848044bda042e4f543d9267ecd87ec
xyyx <xyyx@mail.ru>
2018-01-19 09:53:34 +0800

    whyred: Vendor: Add persist.radio.aosp_usr_pref_sel for Telephony * Fixes the bug in dual SIM device where preferred data selection didn't stick after reboot

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -233,6 +233,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.rat_on=combine
 
+# Make data selection stick in dual SIM devices
+persist.radio.aosp_usr_pref_sel=true
+
 # the level of data wiping under PRESET mode
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.regional.wipedata.level=all
